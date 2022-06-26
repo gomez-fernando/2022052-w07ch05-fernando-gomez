292f6ed26e7c1a3f0a16237bbde1a6898cb978b3
gomez-fernando <ferdgomez@hotmail.es>
2022-06-26 13:58:11 +0200

    Save the robot in the server is ok

--- client/src/pages/create/index.tsx
+++ client/src/pages/create/index.tsx
@@ -1,8 +1,11 @@
 import { SyntheticEvent, useState } from "react";
 import { useDispatch } from "react-redux";
+import { iRobot } from "../../interfaces/interfaces";
+import { ApiRobot } from "../../services/robot/api";
 
 export default function Create(){
     const dispatch = useDispatch();
+    const apiRobot = new ApiRobot()
 
     const [formData, setFormData] = useState({
         name: '',
@@ -18,12 +21,14 @@ export default function Create(){
         //     element.type === "checkbox" ? element.checked : element.value;
         const value = element.value;
         setFormData({...formData, [element.name]: value})
-        console.log(formData);
     };
 
-    const handleSubmit = (ev: SyntheticEvent) => {
+    const handleSubmit = async (ev: SyntheticEvent) => {
         ev.preventDefault();
-        console.log('Guardando:  ', formData);
+        // console.log('Guardando:  ', formData);
+        let newRobot: iRobot = {...formData};
+        newRobot = await apiRobot.setOne(newRobot)
+        console.log(newRobot);
 
     }
 
